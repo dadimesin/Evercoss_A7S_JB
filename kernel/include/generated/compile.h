17d57086075f3be01508e76e22fb947b93430409
hyperion70 <bmaximov71@gmail.com>
2014-04-17 08:36:56 +0400

    first commit

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Thu Apr 17 06:52:46 MSK 2014"
+#define LINUX_COMPILE_BY "hyperion"
+#define LINUX_COMPILE_HOST "root"
+#define LINUX_COMPILER "gcc version 4.6.3 (GCC) "
---
+++ kernel/include/generated/utsrelease.h
@@ -0,0 +1,3 @@
+#define UTS_RELEASE "3.4.5"
+#define BUILD_INFO "SW16_FLY_IQ4404_2014_01_20"
+#define BUILD_FINGERPRINT "wiko"
